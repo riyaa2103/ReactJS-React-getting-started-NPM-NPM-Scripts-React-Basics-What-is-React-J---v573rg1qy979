3bcf9a58dcb5ef3507d23c4bb47b1beea5586bbc
riyaa2103 <105063771+riyaa2103@users.noreply.github.com>
2022-07-02 23:37:48 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ import './SubmitButton.js';
  Const App = ()=> {
   return (
     <div id="main">
-      <Heading/>
+      < Heading/>
       < InputQuery/>
       < SubHeading/>
       < SubmitButton/>
