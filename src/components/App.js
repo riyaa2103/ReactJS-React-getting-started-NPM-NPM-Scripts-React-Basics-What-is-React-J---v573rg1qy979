d022d46e2a428b80abd827e5074c6261ee2a6962
riyaa2103 <105063771+riyaa2103@users.noreply.github.com>
2022-07-02 23:13:53 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import React, { Component, useState } from "react";
-import '../styles/App.css';
-import '../components/Heading.js';
-import '../components/InputQuery.js';
-import '../components/SubHeading.js';
-import '../components/SubmitButton.js';
+import './styles/App.css';
+import './components/Heading.js';
+import './components/InputQuery.js';
+import './components/SubHeading.js';
+import './components/SubmitButton.js';
 
 function App() {
   return (
